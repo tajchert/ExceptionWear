c715f055172941b54ec16e9a1b69ab2dd61d44fb
Michal Tajchert <michal.tajchert@polidea.com>
2015-01-29 13:35:03 +0100

    Better way of sending exceptions.

--- ExceptionWear/src/main/java/pl/tajchert/exceptionwear/ExceptionService.java
+++ ExceptionWear/src/main/java/pl/tajchert/exceptionwear/ExceptionService.java
@@ -3,72 +3,32 @@ package pl.tajchert.exceptionwear;
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.Build;
+import android.util.Log;
 
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.DataMap;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.NodeApi;
-import com.google.android.gms.wearable.Wearable;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
+
+import pl.tajchert.exceptionwear.wear.SendByteArrayToNode;
 
 
 public class ExceptionService extends IntentService {
+    private static final String TAG = "ExceptionService";
+    private ByteArrayOutputStream bos;
+    private ObjectOutputStream oos;
+
 
     public ExceptionService() {
         super("ErrorService");
     }
 
-    private List<String> getNodes(GoogleApiClient mGoogleApiClient) {
-        ArrayList<String> results= new ArrayList<String>();
-        NodeApi.GetConnectedNodesResult nodes =
-                Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
-        for (Node node : nodes.getNodes()) {
-            results.add(node.getId());
-        }
-        return results;
-    }
-
     @Override
     protected void onHandleIntent(Intent intent) {
 
-        GoogleApiClient mGoogleAppiClient = new GoogleApiClient.Builder(ExceptionService.this)
-                .addApi(Wearable.API)
-                .build();
-        mGoogleAppiClient.blockingConnect();
-
-        List<String> nodes = getNodes(mGoogleAppiClient);
-
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = null;
-
         try {
-            oos = new ObjectOutputStream(bos);
-            oos.writeObject(intent.getSerializableExtra("exception"));
-
-            byte[] exceptionData = bos.toByteArray();
-            DataMap dataMap = new DataMap();
-
-            // Add a bit of information on the Wear Device to pass a long with the exception
-            dataMap.putString("board", Build.BOARD);
-            dataMap.putString("fingerprint", Build.FINGERPRINT);
-            dataMap.putString("model", Build.MODEL);
-            dataMap.putString("manufacturer", Build.MANUFACTURER);
-            dataMap.putString("product", Build.PRODUCT);
-
-            dataMap.putByteArray("exception", exceptionData);
-
-            // "Fire and forget" send to connected Smartphone/Tablet using the Wearable Message API
-            if(nodes != null && nodes.size() > 0){
-                Wearable.MessageApi.sendMessage(mGoogleAppiClient, nodes.get(0), "/exceptionwear/wear_error", dataMap.toByteArray());
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            new SendByteArrayToNode(createExceptionInformation(intent).toByteArray(), ExceptionService.this).start();
         } finally {
             try {
                 if (oos != null)
@@ -82,5 +42,30 @@ public class ExceptionService extends IntentService {
                 // ignore close exception
             }
         }
+    }
+
+    private DataMap createExceptionInformation(Intent intent){
+
+        bos = new ByteArrayOutputStream();
+        try {
+            oos = new ObjectOutputStream(bos);
+            oos.writeObject(intent.getSerializableExtra("exception"));
+        } catch (IOException e) {
+            Log.e(TAG, "createExceptionInformation error while getting exception information.");
+        }
+
+        byte[] exceptionData = bos.toByteArray();
+        DataMap dataMap = new DataMap();
+
+        // Add a bit of information on the Wear Device to pass a long with the exception
+        dataMap.putString("board", Build.BOARD);
+        dataMap.putString("fingerprint", Build.FINGERPRINT);
+        dataMap.putString("model", Build.MODEL);
+        dataMap.putString("manufacturer", Build.MANUFACTURER);
+        dataMap.putString("product", Build.PRODUCT);
+
+        dataMap.putByteArray("exception", exceptionData);
+
+        return dataMap;
     }
 }
--- ExceptionWear/src/main/java/pl/tajchert/exceptionwear/wear/WearExceptionTools.java
+++ ExceptionWear/src/main/java/pl/tajchert/exceptionwear/wear/WearExceptionTools.java
@@ -3,7 +3,7 @@ package pl.tajchert.exceptionwear.wear;
 
 public class WearExceptionTools {
     public static final String EXCEPTION_WEAR_TAG = "ExceptionWearTag";
-    public final static String MESSAGE_EXCEPTION_PATH = "pl.tajchert.excetionwear.exception.";
+    public final static String MESSAGE_EXCEPTION_PATH = "/exceptionwear/wear_error";
 
     public static final long CONNECTION_TIME_OUT_MS = 100;
 
